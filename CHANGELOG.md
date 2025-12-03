0132d2e7abf95a6daf15ba6c84b87e2235717802
Julian Stephens <julian@julianstephens.net>
2025-12-03 15:02:44 -0500

    fix: add node shebang to cli build

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.1.1
+
+- Fixes issue where CLI could not be executed globally. A missing shebang (`#!/usr/bin/env node`) was added to the build output, resolving permission and command-not-found errors.
+
 ## v0.1.0
 
 - Adds `coolify-deploy` package
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ["src/index.ts", "src/cli.ts"],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  format: ["esm"],
-  target: "node20",
-  dts: true,
-});
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    format: ['esm'],
+    target: 'node20',
+    dts: true,
+  },
+  {
+    entry: ['src/cli.ts'],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    format: ['esm'],
+    target: 'node20',
+    banner: {
+      js: '#!/usr/bin/env node',
+    },
+  },
+]);
+
